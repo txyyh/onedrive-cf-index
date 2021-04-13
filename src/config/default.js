05e0ed753c73b8a25358b4c05084471677f9a18a
童心音乐会 <zjh330726@gmail.com>
2021-04-13 16:11:06 +0800

    Update private config

--- src/config/default.js
+++ src/config/default.js
@@ -18,7 +18,7 @@ const config = {
   },
 
   refresh_token: REFRESH_TOKEN,
-  client_id: '6600e358-9328-4050-af82-0af9cdde796b',
+  client_id: 'a9abf29f-6bc5-4ee3-8a05-a4c21df3f264',
   client_secret: CLIENT_SECRET,
 
   /**
@@ -29,7 +29,7 @@ const config = {
   /**
    * The base path for indexing, all files and subfolders are public by this tool. For example: `/Public`.
    */
-  base: '/Public',
+  base: '/Anime',
 
   /**
    * Feature: Pagination when a folder has multiple(>${top}) files
